c92055b5077a57ce0edac61512a5eca971722eae
DDao Lee <dlekgh3576@naver.com>
2021-11-22 19:08:27 +0900

    ConnectionMgr - enableWifi, disableWifi 추가 SockClient - recv, send 추가

--- app/src/main/java/com/example/elevator/ConnectionMgr.java
+++ app/src/main/java/com/example/elevator/ConnectionMgr.java
@@ -1,55 +1,170 @@
 package com.example.elevator;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.net.NetworkSpecifier;
+import android.net.Uri;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.text.TextUtils;
+import android.net.wifi.WifiNetworkSpecifier;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ConnectionMgr extends MainActivity {
-    String enableWifi(){
+public class ConnectionMgr extends AppCompatActivity {
 
-            /*
-            String WIFI_NAME = "";
-            String WIFI_PASSWORD = "";
-            WifiConfiguration wificonfig = new WifiConfiguration();
-            wificonfig.SSID = String.format("\"%s\"", WIFI_NAME);
-            wificonfig.preSharedKey = String.format("\"%s\"", WIFI_PASSWORD);
+    Context context;
 
-            WifiManager wifiManagerAboutConnect =(WifiManager)getApplicationContext().getSystemService(WIFI_SERVICE);
-            int netId = wifiManagerAboutConnect.addNetwork(wificonfig);
-            wifiManagerAboutConnect.disconnect();
-            wifiManagerAboutConnect.enableNetwork(netId, true);
-            wifiManagerAboutConnect.reconnect();
-            */
-
-
-        WifiManager wifiManager = (WifiManager)getApplicationContext( ).getSystemService(WIFI_SERVICE);
-        wifiManager.setWifiEnabled(true);
+    static final int PERMISSIONS_REQUEST = 0x0000001;
+    private ConnectivityManager connectivityManager;
+    private ConnectivityManager.NetworkCallback networkCallback;
 
 
-        String ssid = null;
+    void connectWifi() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
-
-        if (networkInfo.isConnected()) {
-
-            WifiManager wifiManagerAboutSSID = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-            WifiInfo connectionInfo = wifiManagerAboutSSID.getConnectionInfo();
-            if (connectionInfo != null && !TextUtils.isEmpty(connectionInfo.getSSID())) {
-                ssid = connectionInfo.getSSID();
+        try {
+            if (!wifiManager.isWifiEnabled()) {
+                wifiManager.setWifiEnabled(true);
             }
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+
+                WifiNetworkSpecifier.Builder builder = new WifiNetworkSpecifier.Builder();
+                builder.setSsidPattern(new PatternMatcher("CarKey",PatternMatcher.PATTERN_PREFIX));
+                builder.setWpa2Passphrase("1234qqqq");
+
+                WifiNetworkSpecifier wifiNetworkSpecifier = builder.build();
+
+                final NetworkRequest.Builder networkRequestBuilder = new NetworkRequest.Builder();
+                networkRequestBuilder.addTransportType(NetworkCapabilities.TRANSPORT_WIFI);
+                networkRequestBuilder.setNetworkSpecifier(wifiNetworkSpecifier);
+
+                NetworkRequest networkRequest = networkRequestBuilder.build();
+                networkCallback = new ConnectivityManager.NetworkCallback() {
+                    @Override
+                    public void onAvailable(@NonNull Network network) {
+                        super.onAvailable(network);
+                        connectivityManager.bindProcessToNetwork(network);
+                        Toast.makeText(getApplicationContext(), "연결됨", Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+                connectivityManager.registerNetworkCallback(networkRequest, networkCallback);
+                connectivityManager.requestNetwork(networkRequest, networkCallback);
+
+
+            } else {
+                WifiConfiguration wifiConfiguration = new WifiConfiguration();
+                wifiConfiguration.SSID = String.format("\"%s\"", "wifi 이름"); // 연결하고자 하는 SSID
+                wifiConfiguration.preSharedKey = String.format("\"%s\"", "비밂번호"); // 비밀번호
+                int wifiId = wifiManager.addNetwork(wifiConfiguration);
+                wifiManager.enableNetwork(wifiId, true);
+                Toast.makeText(getApplicationContext(), "연결됨", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "연결 예외 : " + e.toString(), Toast.LENGTH_SHORT).show();
         }
 
-        return ssid;
+    }
+    void Disconnect() {
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        try {
+            if (wifiManager.isWifiEnabled()) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    connectivityManager.unregisterNetworkCallback(networkCallback);
+                    Toast.makeText(getApplicationContext(), "연결 끊김", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    if (wifiManager.getConnectionInfo().getNetworkId() == -1) {
+                        Toast.makeText(getApplicationContext(), "연결", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        int networkId = wifiManager.getConnectionInfo().getNetworkId();
+                        wifiManager.removeNetwork(networkId);
+                        wifiManager.saveConfiguration();
+                        wifiManager.disconnect();
+                        Toast.makeText(getApplicationContext(), "연결 끊김", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            } else
+                Toast.makeText(getApplicationContext(), "Wifi 꺼짐", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(), "연결 해제 예외 : " + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void OnCheckPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(this, "앱 실행을 위해서는 권한을 설정해야 합니다", Toast.LENGTH_LONG).show();
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        PERMISSIONS_REQUEST);
+            }
+        }
     }
 
-    public void disableWifi() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "앱 실행을 위한 권한이 설정 되었습니다", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "앱 실행을 위한 권한이 취소 되었습니다", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 
-        WifiManager wifiManager = (WifiManager)getApplicationContext( ).getSystemService(WIFI_SERVICE);
-        wifiManager.setWifiEnabled(false);
+    public boolean checkSystemPermission() {
+
+        boolean permission = true;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   //23버전 이상
+            permission = Settings.System.canWrite(this);
+            if (permission) {
+            } else {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 2127);
+                permission = false;
+            }
+        } else {
+
+        }
+
+        return permission;
     }
 }
--- app/src/main/java/com/example/elevator/sock/SockClient.java
+++ app/src/main/java/com/example/elevator/sock/SockClient.java
@@ -1,119 +1,132 @@
 package com.example.elevator.sock;
 
-public class SockClient {final int port = 5000;
-	    final String serverIp = "192.168.5.5"; 
-	
-	    Socket socket;
-	    JSONObject giveObj = new JSONObject();
-	    JSONObject obj = new JSONObject();
-	    JSONArray objArray = new JSONArray();
-	
-	    void send(JSONObject obj){
-	        try {
-	            socket = new Socket(serverIp, port);
-	
-	            OutputStream os = socket.getOutputStream();
-	            
-	            byte cmd = (byte)obj.get("cmd");
-	            byte length = (byte)obj.get("length");
-	
-	            byte[] reqBuffer = new byte[7];
-	
-	            reqBuffer[0] = (byte)0xA5; //STX_1
-	            reqBuffer[1] = (byte)0x5A; //STX_2
-	            reqBuffer[2] = length; //length_Low
-	            reqBuffer[3] = (byte)0x00; //length_High
-	            reqBuffer[4] = cmd; //Command
-	            reqBuffer[5] = (byte)0x46; //CRC_Low
-	            reqBuffer[6] = (byte)0xB1; //CRC_High
-	
-	            os.write(reqBuffer);
-	            os.flush();
-	        } catch (Exception e) {
-	        	e.printStackTrace();
-	        }
-	    }
-	    
-	    JSONObject recv() {
-	        int size = 1024;
-	        
-	        try {
-	            InputStream is = socket.getInputStream();
-	            
-        		
-	            byte[] recvBuffer = new byte[size];
-	            int recvSize = is.read(recvBuffer);
-	            
-	            String cmd = String.valueOf(recvBuffer[4]);
-	            
-	            int idSize = 7;
-	            int IDStartIndex = 5;
-	            String ID = "";
-	            
-	            for(int i = IDStartIndex; i <idSize; i++) {
-	                ID = ID + String.valueOf(recvBuffer[i]);
-	            }
-	            
-	            while(true) {
-		         
-		            int datatimeStartIndex = 14;
-		            String datetime = "";
-		            String date = "";
-		            String time = "";
-		            short code = 0;
-		            
-		            short errorcode = 0;
-		            errorcode |= ((short)recvBuffer[13] << 8) & 0xFF00;
-	        		errorcode |= ((short)recvBuffer[12]) & 0x00FF;
-	        		
-	        		recvSize = recvSize -((125 - errorcode) * 8) - 10;
-	        		
-		            for(int i = datatimeStartIndex; i < recvSize; i = i + 8 ) {
-		            	for(int j = 0; j < 3; j++) {
-		            		if (recvBuffer[i + j] < 10) { date = date + "0" + recvBuffer[i + j]; }
-		            		else {date = date + recvBuffer[i + j];}	
-		            
-		            		if(j != 2) { date = date + "-";}
-		            	}
-		                
-		            	for(int j = 3; j < 6; j++) {
-		            		if (recvBuffer[i + j] < 10) { time = time + "0" + recvBuffer[i + j]; }
-		            		else {time = time + recvBuffer[i + j];}	
-		            
-		            		if(j != 5) { time = time + ":";}
-		            	}
-		            	
-		            	datetime = date + " " + time;
-		            	
-		        		code |= ((short)recvBuffer[i + 7] << 8) & 0xFF00;
-		        		code |= ((short)recvBuffer[i + 6]) & 0x00FF;
-		        		
-		                
-		        	    JSONObject obj = new JSONObject();
-		                obj.put("datetime",datetime);
-		                obj.put("code", code);
-		                objArray.add(obj);
-		
-		                date = time =  "";
-		                code = 0;
-		                
-		            }
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
-	        		if(errorcode == 125) {
-	        			recvSize = is.read(recvBuffer);
-	        		}
-	        		else {
-	        			break;
-	        		}
-	            }
-	            
-	            giveObj.put("cmd", cmd);
-	            giveObj.put("ID", ID);
-	            giveObj.put("info", objArray);
-	        } catch (Exception e) {
-	            System.out.println("2실패");
-	        }
-	
-	        return giveObj;
-	    }
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class SockClient {
+
+	final int port = 5000;
+	final String serverIp = "192.168.5.5";
+
+	Socket socket;
+	JSONObject giveObj = new JSONObject();
+	JSONObject obj = new JSONObject();
+	JSONArray objArray = new JSONArray();
+
+	void send(JSONObject obj){
+		try {
+			socket = new Socket(serverIp, port);
+
+			OutputStream os = socket.getOutputStream();
+
+			byte cmd = (byte)obj.get("cmd");
+			byte length = (byte)obj.get("length");
+
+			byte[] reqBuffer = new byte[7];
+
+			reqBuffer[0] = (byte)0xA5; //STX_1
+			reqBuffer[1] = (byte)0x5A; //STX_2
+			reqBuffer[2] = length; //length_Low
+			reqBuffer[3] = (byte)0x00; //length_High
+			reqBuffer[4] = cmd; //Command
+			reqBuffer[5] = (byte)0x46; //CRC_Low
+			reqBuffer[6] = (byte)0xB1; //CRC_High
+
+			os.write(reqBuffer);
+			os.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	JSONObject recv() {
+		int size = 1024;
+		int recvSize = 0;
+		int start_id_index = 5;
+		int id_size = 7;
+		int errorcode_index = 13;
+		int start_datetime_index = 14;
+		short code = 0;
+		short errorcode = 0;
+
+		String cmd = "";
+		String ID = "";
+
+		String datetime = "";
+		String date = "";
+		String time = "";
+
+		try {
+			InputStream is = socket.getInputStream();
+
+			byte[] recvBuffer = new byte[size];
+			recvSize = is.read(recvBuffer);
+
+			cmd = String.valueOf(recvBuffer[4]);
+
+			for(int i = start_id_index; i <id_size; i++) {
+				ID += String.valueOf(recvBuffer[i]);
+			}
+
+			while(true) {
+
+				datetime = "";
+				date = "";
+				time = "";
+				code = 0;
+
+				errorcode = 0;
+				errorcode |= ((short)recvBuffer[errorcode_index] << 8) & 0xFF00;
+				errorcode |= ((short)recvBuffer[errorcode_index - 1]) & 0x00FF;
+
+				recvSize = recvSize -((125 - errorcode) * 8) - 10;
+
+				for(int i = start_datetime_index; i < recvSize; i = i + 8 ) {
+					for(int j = 0; j < 3; j++) {
+						if (recvBuffer[i + j] < 10) { date = date + "0" + recvBuffer[i + j]; }
+						else {date += recvBuffer[i + j];}
+
+						if(j != 2) { date += "-";}
+					}
+
+					for(int j = 3; j < 6; j++) {
+						if (recvBuffer[i + j] < 10) { time = time + "0" + recvBuffer[i + j]; }
+						else {time += recvBuffer[i + j];}
+
+						if(j != 5) { time += ":";}
+					}
+
+					datetime = date + " " + time;
+
+					code |= ((short)recvBuffer[i + 7] << 8) & 0xFF00;
+					code |= ((short)recvBuffer[i + 6]) & 0x00FF;
+
+					JSONObject obj = new JSONObject();
+					obj.put("datetime",datetime);
+					obj.put("code", code);
+					objArray.put(obj);
+				}
+
+				if(errorcode == 125) {
+					is.read(recvBuffer);
+				}
+				else {
+					break;
+				}
+			}
+
+			giveObj.put("cmd", cmd);
+			giveObj.put("ID", ID);
+			giveObj.put("info", objArray);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return giveObj;
+	}
 }
